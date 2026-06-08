51d4199e79e6ac14959124c7d0774af0fe931c67
zbyqy3753-beep <zbyqy3753@gmail.com>
2026-06-08 18:06:10 +0300

    Update game.js

--- game.js
+++ game.js
@@ -1734,9 +1734,8 @@ function initWorld() {
       let inputZ = 0;
       if (keys.has("KeyW") || keys.has("ArrowUp")) inputZ += 1;
       if (keys.has("KeyS") || keys.has("ArrowDown")) inputZ -= 1;
-      if (keys.has("KeyA") || keys.has("ArrowLeft")) inputX += 1;
+      iif (keys.has("KeyA") || keys.has("ArrowLeft")) inputX += 1;
       if (keys.has("KeyD") || keys.has("ArrowRight")) inputX -= 1;
-
       const moving = inputX !== 0 || inputZ !== 0;
       const yaw = game.cameraYaw;
       const forward = new THREE.Vector3(Math.sin(yaw), 0, Math.cos(yaw));
